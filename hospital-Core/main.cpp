912031ff4ade3fd84c8803ef945ba0b677dfe156
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2019-10-24 14:46:25 -0500

    Conexion base de datos
    se realizo la conexion de la base de datos con odbc

--- hospital-Core/main.cpp
+++ hospital-Core/main.cpp
@@ -1,8 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtSql/QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
+    QSqlDatabase mDatabase = QSqlDatabase::addDatabase("QODBC","Connection");
+    mDatabase.setDatabaseName("hospital");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
--- hospital-Core/mainwindow.cpp
+++ hospital-Core/mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSqlDatabase>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    mDatabase = QSqlDatabase::database("Connection");
+    if(!mDatabase.isOpen()){
+           qDebug()<<"ERROR";
+    }else{
+            qDebug()<<"Base de datos conectada exitosamente";
+    }
 }
 
 MainWindow::~MainWindow()
