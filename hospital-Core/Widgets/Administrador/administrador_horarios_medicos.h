861126dfeca331d163d43c5b776440bf6b236c22
Daniel Garnica Sanchez <thedaniel115@gmail.com>
2019-11-12 05:59:15 -0600

    Horario Medicos LISTO

--- hospital-Core/Widgets/Administrador/administrador_horarios_medicos.h
+++ hospital-Core/Widgets/Administrador/administrador_horarios_medicos.h
@@ -2,7 +2,8 @@
 #define ADMINISTRADOR_HORARIOS_MEDICOS_H
 
 #include <QWidget>
-
+#include <QSqlDatabase>
+#include <QSqlQuery>
 namespace Ui {
 class administrador_horarios_medicos;
 }
@@ -14,9 +15,16 @@ class administrador_horarios_medicos : public QWidget
 public:
     explicit administrador_horarios_medicos(QWidget *parent = nullptr);
     ~administrador_horarios_medicos();
+    void limiparCatalogo();
+    void inicializarCatalogo();
+private slots:
+    void on_comboBox_ID_currentIndexChanged(int index);
+
+    void on_dateEdit_userDateChanged(const QDate &date);
 
 private:
     Ui::administrador_horarios_medicos *ui;
+    QSqlDatabase mDatabase;
 };
 
 #endif // ADMINISTRADOR_HORARIOS_MEDICOS_H
