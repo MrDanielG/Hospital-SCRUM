2444394fab873d00cb5c9d4f75f54a1f68903f5e
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2019-12-05 01:57:02 -0600

    Ingles y registro

--- hospital-Core/main.cpp
+++ hospital-Core/main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QtSql/QSqlDatabase>
+#include <QTranslator>
+#include <QLibraryInfo>
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +18,11 @@ int main(int argc, char *argv[])
 
     mDatabase.setDatabaseName("hospital");
     QApplication a(argc, argv);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(),
+            QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    a.installTranslator(&qtTranslator);
     MainWindow w;
     w.show();
 
